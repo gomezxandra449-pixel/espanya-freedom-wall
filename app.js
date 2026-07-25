a75f5993648e075b1512ec3030bb677d20d7af7a
gomezxandra449-pixel <gomezxandra449@gmail.com>
2026-07-26 04:05:54 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 alert("1. app.js loaded");
 
-const SUPABASE_URL = "https://sjyjiphjllvnswzgsvnw.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeWppcGhqbGx2bnN3emdzbndrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5OTExMzYsImV4cCI6MjEwMDU2NzEzNn0.0oFmsAsVHB96RgOs33sCTYDPkCCH0Jxdl-vDN8HOc1E";
-
-alert("2. Before createClient");
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
-
-alert("3. After createClient");
-
-async function sendLetter() {
-  alert("4. sendLetter() called");
-
-  const title = document.getElementById("title").value.trim();
-  const message = document.getElementById("message").value.trim();
-
-  alert("5. Title: " + title);
-}
+document.querySelector("button").addEventListener("click", function () {
+    alert("2. Button works!");
+});
