72fe9b50cf317c4247e9d5e901b6d5adb376613f
gomezxandra449-pixel <gomezxandra449@gmail.com>
2026-07-26 04:16:56 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 alert("1. app.js loaded");
 
-const btn = document.getElementById("sendBtn");
+const SUPABASE_URL = "https://sjyjiphjllvnswzgsvnw.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeWppcGhqbGx2bnN3emdzbndrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5OTExMzYsImV4cCI6MjEwMDU2NzEzNn0.0oFmsAsVHB96RgOs33sCTYDPkCCH0Jxdl-vDN8HOc1E";
 
-if (btn) {
-  alert("2. Found the button!");
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 
-  btn.addEventListener("click", function () {
-    alert("3. Button clicked!");
-  });
-} else {
-  alert("Button NOT found!");
+async function sendLetter() {
+  alert("2. sendLetter started");
+
+  const title = document.getElementById("title").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  alert("3. Title = " + title);
+
+  const { data, error } = await supabase
+    .from("letters")
+    .insert([
+      {
+        title: title,
+        message: message
+      }
+    ]);
+
+  if (error) {
+    alert("ERROR: " + error.message);
+  } else {
+    alert("SUCCESS!");
+  }
 }
